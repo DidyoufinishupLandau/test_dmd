8a68c8c7e59cc74cb23399dc958e309782297753
DidyoufinishupLandau <86139044+DidyoufinishupLandau@users.noreply.github.com>
2023-11-17 03:02:24 +0000

    Add files via upload

--- control_dmd.py
+++ control_dmd.py
@@ -5,15 +5,14 @@ import generate_pattern
 
 
 call_pattern = generate_pattern.DmdPattern('hadamard', 128,128, gray_scale=255)
-hadamard_pattern, _ = call_pattern.execute(length=1)
+hadamard_pattern, _ = call_pattern.execute(length=0.5)
 print(len(hadamard_pattern))
 print("here")
 #hadamard_pattern = generate_pattern.embed(hadamard_pattern)
 
 # define a 3D array with form [[mask], [mask], [mask]...]
-cd = control_DMD(hadamard_pattern, project_name = "my_project", group = 44, main_sequence_itr=1, frame_time=1)
-for i in range(1):
-    cd.execute()
+cd = control_DMD(hadamard_pattern, project_name = "my_project", group =  17, main_sequence_itr=1, frame_time=5)
+cd.execute()
 
 """cd = control_DMD(inverse_patter, project_name = "my_project", main_sequence_itr=1, frame_time=10)
 for i in range(1, 2):
--- pico_control.py
+++ pico_control.py
@@ -1,4 +1,3 @@
-import os
 import time
 from machine import Pin, ADC
 import utime
@@ -12,183 +11,115 @@ TO_DMD_IN_pin: Pin = Pin(1, Pin.OUT)
 # PD_pin is connected to ADC0
 PD_pin: ADC = ADC(0)
 PD_pin_two: ADC = ADC(1)
-
+led = Pin(25, Pin.OUT, Pin.PULL_DOWN)
+led.toggle()
 # Global variables
-_NO_OF_IMAGES: int = 4096
-image_size = 128
-_DELAY: int = 0
 _START: bool = False
-_READY_FOR_ACQ: bool = False
+_BREAK: bool = False
 _DATA: list = []
 _DATA_two: list = []
-_ACQ_COUNTER: int = 0
-_Data_counter = 0
-NUM_GROUP = 1
-group = 43
+i = 0
 
 
 def handle_interrupt(interrupt_pin: Pin) -> None:
     """Define interrupt  handler - must be fast and inner loop"""
-    global _ACQ_COUNTER
     global _DATA
-    # Increment Acquisition handler
-    _ACQ_COUNTER += 1
-    #print(sys.stdin.buffer.read(read_PD()))
+    global _DATA_two
     _DATA.append(read_PD())
     _DATA_two.append(read_PD_two())
-def stop_interrupt(interrupt_pin: Pin):
-    global _ACQ_COUNTER
+
+def test_interrupt(pin):
+    global i
     global _DATA
+    global _DATA_two
+    _DATA.append(i)
+    _DATA_two.append(i)
+    i+=1
+
+def test_acquire():
+    led.irq(trigger=Pin.IRQ_RISING, handler=test_interrupt)
+
+def test_disable():
+    led.irq(handler=None)
     
-    print(_ACQ_COUNTER)
 def read_PD() -> float:
     """Read from the photodiode pin, return as u16"""
 
     return PD_pin.read_u16()
+def simu_signal():
+    for i in range(4096):
+        led.value(1)
+        led.toggle()
+        utime.sleep_us(10)
+        led.value(0)
+        led.toggle()
 def read_PD_two():
     return PD_pin_two.read_u16()
 
-
-def send_trigger(sleep_time_us: int = 10):
-    """Send the output trigger high and low"""
-    TO_DMD_IN_pin.value(1)
-    # Define sleep-time
-    if sleep_time_us > 0:
-        utime.sleep_us(sleep_time_us)
-    TO_DMD_IN_pin.value(0)
-
-def acquire(no_of_images: int, delay: int = 0) -> list:
+def acquire() -> list:
     """Primary acquire loop"""
-    global _READY_FOR_ACQ
-    global _DATA
-    global _ACQ_COUNTER
-    global _DATA_two
-    global _Data_counter
     # Attach handle_interrupt to the falling edge of Pin
     FROM_DMD_OUT_pin.irq(trigger=Pin.IRQ_FALLING, handler=handle_interrupt)
-
-    _DATA = []
-    _ACQ_COUNTER = 0
-    if delay > 0:
-        while _ACQ_COUNTER < no_of_images + 1:
-            send_trigger()
-            utime.sleep_us(delay)
-
-    else:
-        print("no delay acquire")
-            #handle_interrupt(0)
-        while _ACQ_COUNTER < no_of_images:
-            send_trigger()
-        FROM_DMD_OUT_pin.irq(trigger=Pin.IRQ_FALLING, handler=stop_interrupt)
-        if _ACQ_COUNTER == no_of_images:
-            print("saving_image")
-            print(len(_DATA))
-            print(len(_DATA_two))
-            _Data_counter+=1
-            name_one = f"{image_size}_{group}_one_data_{_Data_counter}.csv"
-            name_two = f"{image_size}_{group}_two_data_{_Data_counter}.csv"
-            str_data = str(_DATA)
-            str_data = str_data[1:]
-            str_data = str_data[:len(str_data)-1]
-            str_data_two = str(_DATA_two)
-            str_data_two = str_data_two[1:]
-            str_data_two = str_data_two[:len(str_data_two)-1] 
-            with open("lock.file", 'w') as lock:
-                pass
-            with open(name_one, 'w') as file_one:
-                file_one.write(str_data)
-            file_one.close()
-            with open(name_two, 'w') as file_two:
-                file_two.write(str_data_two)
-            file_two.close()
-        print("acquire_complete")
-        _DATA = []
-        _DATA_two = []
-        _ACQ_COUNTER = 0
-        print(_Data_counter)
-        
+    
     return None
 
+
+def disable_acquire():
+    FROM_DMD_OUT_pin.irq(handler=None)
 
 def Read() -> str:
     return sys.stdin.readline().encode('UTF-8')
 
 
-def Write(data: list):
-    if len(_DATA) > _NO_OF_IMAGES:
-        for i in range(_NO_OF_IMAGES):
-            print(_DATA[i + 1])
-    print("END")
-
-
 def restart():
-    global _NO_OF_IMAGES
-    global _DELAY
     global _START
     global _DATA
     global _DATA_two
-    global _ACQ_COUNTER
-    global _Data_counter
-    _NO_OF_IMAGES = 4096
-    _DELAY = 0
     _START = False
     _DATA = []
     _DATA_two = []
-    _ACQ_COUNTER = 0
-    _Data_counter = 0
     
 def commands_parser(comm: str) -> None:
-    global _NO_OF_IMAGES
-    global _DELAY
     global _START
     global _DATA
+    global _DATA_two
+    global i
+    global _BREAK
 
     led = Pin(25, Pin.OUT)
-    if "N_" in comm:
-        # Set the number of images
-        _NO_OF_IMAGES = int(comm.replace("N_", ""))
-    if "D_" in comm and not ("LED_" in comm):
-        # Set the internal delay
-        _DELAY = int(comm.replace("D_", ""))
     if "S_" in comm:
         # Set the start flag
         if "TRUE" in comm:
             _START = True
         elif "FALSE" in comm:
             _START = False
-    # Helper function
+
+    
     if "LED_ON" in comm:
         led.high()
     if "LED_OFF" in comm:
         led.low()
 
-    if "INFO" in comm:
-        print("Number of images: ", _NO_OF_IMAGES)
-        print("Delay after a single data acquisition (us): ", _DELAY)
-        print("Data size: ", len(_DATA))
-        print("Data size two: ", len(_DATA_two))
-
-    # GD = Get Data
-    if "GD" in comm:
-        Write(_DATA)
-    # Reset Data
-
-    if "RD" in comm:
+    if "ShowData" in comm:
+        for i in range(4096):
+            print(_DATA[i])
+            print(_DATA_two[i])
+    if "RS" in comm:
         _DATA = []
-    if "ShowDataOne" in comm:
-        print(_DATA)
-    if "ShowDataTwo" in comm:
-        print(_DATA_two)
-    if "C" in comm:
-        restart()
-    if "*IDN" in comm:
-        print("PicoADC v1.0")
-
-
+        _DATA_two = []
+        i = 0
+    if "SM" in comm:
+        simu_signal()
+    if "STOP" in comm:
+        test_disable()
+    if "BREAK" in comm:
+        _BREAK = True
 def main():
     """Main loop"""
     global _START
+    global _BREAK
     _START = False
+    _BREAK = False
     # Continuously read
     while True:
         text = Read()
@@ -196,7 +127,9 @@ def main():
             # Parse
             commands_parser(text)
         if _START:
-            for i in range(NUM_GROUP):
-                acquire(_NO_OF_IMAGES, _DELAY)
+            test_acquire()
             _START = False
-main()
+        if _BREAK:
+            machine.reset()
+if __name__ == "__main__":
+    main()
