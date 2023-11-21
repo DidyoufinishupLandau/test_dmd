30aae2d603b16ad973960a5b9882b32b291e5595
zhangpan <13110712+pzzzhang@user.noreply.gitee.com>
2023-11-21 00:19:27 +0000

    update stream and adaptive sampling

--- test_wavelet.py
+++ test_wavelet.py
@@ -34,7 +34,7 @@ def embed_in_corner(smaller_matrices, size, position):
         return larger
 
     return list(map(embed, smaller_matrices))
-def split_image(arr):
+def split_matrix(arr):
     width = arr.shape[0]
     top_left, top_right, bot_left, bot_right = (arr[0:int(width/2), 0:int(width/2)],
                                                 arr[0:int(width/2), int(width/2):width],
@@ -81,6 +81,9 @@ def adaptive_pattern(image, size, num_stage):
         image_ = SimulationCompressingImage(image).execute(temp_pattern, temp_inverse)
         wt_level_ = wavelet_transform_level_one(image_)
         temp_wt_list.append(wt_level_)
+
+
+
         if i == 4**j:
             print(i, 4**j)
             j+=1
@@ -89,52 +92,70 @@ def adaptive_pattern(image, size, num_stage):
             wt_dic.update(temp_wt_list)
             temp_wt_list=[]
     return wt_dic
-def test_wavelet():
-    random_image = random_image_with_shapes(128, 128, 10)
-    pattern, inverse = DmdPattern("hadamard", 64,64).execute()
-    plot_pixel(random_image)
-    # calculate wavelet transform
-    image = SimulationCompressingImage(random_image).execute(pattern, inverse)
-    wt_level = wavelet_transform_level_one(image) # 64
+def adaptive_pattern(image, size, num_stage):
+    image_size = image.shape
+    gp = DmdPattern('hadamard', size, size)
+    two_dimension_hadamard, negative_pattern = gp.execute()
+    new_pattern_list = []
+    new_pattern_list.append(two_dimension_hadamard)
+    negative_pattern_list = []
+    negative_pattern_list.append(negative_pattern)
+    # generate all pattern that would require in later procedure
+    # noting that the order is always from top left to bottom right
+    for i in range(1, num_stage+1):
+        for j in range(1,5):
+            for k in range(4 ** (i - 1)):
+                print(int(k + 4 ** (i - 2)), "size", (size*2**(i), size*2**(i)), j)
+                negative_pattern_list.append(
+                    embed_in_corner(negative_pattern_list[int(k + 4 ** (i - 2))],(size*2**(i), size*2** (i)), j))
+                new_pattern_list.append(
+                    embed_in_corner(new_pattern_list[int(k + 4 ** (i - 2))], (size*2**(i), size*2**(i)), j))
+    wt_dic = {}
+    i = 0
+    j = 0
+    temp_wt_list = []
+    projection_list = [1]
+    while len(new_pattern_list)>0:
+        print(len(new_pattern_list))
+        temp_pattern = new_pattern_list[0]
+        temp_inverse = negative_pattern_list[0]
+        del new_pattern_list[0]
+        del negative_pattern_list[0]
+        if projection_list[i]==1:
+            image_ = SimulationCompressingImage(image).execute(temp_pattern, temp_inverse)
+            wt_level_ = wavelet_transform_level_one(image_)
+            temp_wt_list.append(wt_level_)
 
-    plot_pixel(image)
-    #now embed in corner
-    pattern_one = embed_in_corner(pattern, (128,128), 1)
-    inverse_one = embed_in_corner(inverse, (128,128), 1)
-    image_one = SimulationCompressingImage(random_image).execute(pattern_one, inverse_one)
-    wt_level_one = wavelet_transform_level_one(image_one) # 64
-    pattern_two = embed_in_corner(pattern, (128,128), 2)
-    inverse_two = embed_in_corner(inverse, (128,128), 2)
-    image_two = SimulationCompressingImage(random_image).execute(pattern_two, inverse_two)
-    wt_level_two = wavelet_transform_level_one(image_two)
-    pattern_three = embed_in_corner(pattern, (128,128), 3)
-    inverse_three = embed_in_corner(inverse, (128,128), 3)
-    image_three = SimulationCompressingImage(random_image).execute(pattern_three, inverse_three)
-    wt_level_three = wavelet_transform_level_one(image_three)
-    pattern_three = embed_in_corner(pattern, (128,128), 4)
-    inverse_three = embed_in_corner(inverse, (128,128), 4)
-    image_four = SimulationCompressingImage(random_image).execute(pattern_three, inverse_three)
-    wt_level_four = wavelet_transform_level_one(image_four)
-    plot_pixel(image_one+image_two)
-    def combine_wavelet(list_wavelet):
-        image = []
-        horizontal = []
-        vertial = []
-        diagonal = []
-        for i in range(len(list_wavelet)):
-            image.append(list_wavelet[i][0])
-            horizontal.append(list_wavelet[i][1])
-            vertial.append(list_wavelet[i][2])
-            diagonal.append(list_wavelet[i][3])
-        image_arr = np.array(image)
-        horizontal_arr = np.array(horizontal)
-        vertial_arr = np.array(vertial)
-        diagonal_arr = np.array(diagonal)
-        return np.sum(image_arr,axis=0), np.sum(horizontal_arr,axis=0), np.sum(vertial_arr,axis=0),np.sum(diagonal_arr,axis=0)
-    list_wavelet = [wt_level_one, wt_level_four]
-    ca, ch,cv,cd = combine_wavelet(list_wavelet)
-    reconstruction = inverse_wavelet_trasform(ca, ch,cv,cd)
-    plot_pixel(reconstruction)
+        elif projection_list[i]==0:
+            image_ = np.zeros((image_size))
+            wt_level_ = wavelet_transform_level_one(image_)
+            temp_wt_list.append(wt_level_)
+        i+=1
+        if i == 4**j:
+            print(i, 4**j)
+            j+=1
+            i=0
+            temp_wt_dic = {f"{j}": temp_wt_list}
+            wt_dic.update(temp_wt_dic)
+            projection_list = []
+            for m in range(len(temp_wt_list)):
+                print(analyze_wavelet(temp_wt_list[m]))
+                projection_list += analyze_wavelet(temp_wt_list[m])
+            temp_wt_list=[]
+    return wt_dic
+def analyze_wavelet(wavelet, threshold=1):
+    _, hor, ver, dia = wavelet
+    combine_wavelet = hor+ver+dia
+    sliced_matrix = split_matrix(combine_wavelet)
+    projection_index = []
+    for i in range(len(sliced_matrix)):
+        summed_value = np.sum(sliced_matrix[i])
+        print(summed_value)
+        if np.abs(summed_value)<threshold:
+            projection_index.append(0)
+        else:
+            projection_index.append(1)
+    return projection_index
 def reconstruct_image_from_wavelet(wt_dic):
     reconstruction = []
     for i in range(len(wt_dic)):
@@ -144,15 +165,25 @@ def reconstruct_image_from_wavelet(wt_dic):
         for j in range(len(temp_wt)):
             print("j", j)
             image, hor, ver, dia = temp_wt[j]
+            summed_edge = hor+ver+dia
+            print("calculating_edge:", np.sum(summed_edge))
             reconstructed_image = inverse_wavelet_trasform(image, hor, ver, dia)
             temp_reconstruction.append(reconstructed_image)
+        """        if i==2:
+                    del temp_reconstruction[14]
+                    del temp_reconstruction[3]
+                    del temp_reconstruction[2]
+                if i==1:
+                    del temp_reconstruction[2]"""
         reconstruction.append(np.sum(np.array(temp_reconstruction),axis = 0))
     return reconstruction
 random_image = random_image_with_shapes(128,128)
 plot_pixel(random_image)
 wt_list = adaptive_pattern(random_image,32, 2)
+image, hor, ver, dia  = wt_list["2"][2]
 
 reconstruction = reconstruct_image_from_wavelet(wt_list)
 plot_pixel(reconstruction[0])
 plot_pixel(reconstruction[1])
 plot_pixel(reconstruction[2])
+plot_pixel(reconstruction[0]+reconstruction[1]+reconstruction[2])
